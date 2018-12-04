591f17dd5ad3a9d11f20470e3575646c0f0522e1
David Roff <David.Roff@asynchrony.com>
2018-12-04 14:39:04 -0600

    added no collections message to postman collection view

--- mailman/Main/PostmanCollectionView.swift
+++ mailman/Main/PostmanCollectionView.swift
@@ -1,11 +1,24 @@
 import UIKit
 
 class PostmanCollectionView: UIView {
+    private let noCollectionsLabel = UILabel()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         backgroundColor = .white
+        
+        noCollectionsLabel.text = "You have not added any collections yet."
+        noCollectionsLabel.textAlignment = .center
+        noCollectionsLabel.numberOfLines = 0
+        
+        addSubview(noCollectionsLabel)
+        
+        noCollectionsLabel.translatesAutoresizingMaskIntoConstraints = false
+        noCollectionsLabel.topAnchor.constraint(equalTo: topAnchor, constant: 30).isActive = true
+        noCollectionsLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 30).isActive = true
+        noCollectionsLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -30).isActive = true
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
